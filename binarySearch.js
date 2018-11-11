4a412fed99813d39810143258718f0dbd9d07075
DiDex <wc34fun@mail.ru>
2018-11-11 10:52:26 +0800

    binary search v2

--- binarySearch.js
+++ binarySearch.js
@@ -17,4 +17,22 @@ const bynarySearh = (arr, str) => {
 // prettier-ignore
 const arr = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ]; // 10
 
-console.log(bynarySearh(arr, 10));
+console.log(bynarySearh(arr, 19));
+
+const bynarySearh2 = (arr, str) => {
+  let min = 0;
+  let max = arr.length - 1;
+  let guess;
+
+  while (min <= max) {
+    guess = Math.floor((min + max) / 2);
+    if (arr[guess] === str) return guess;
+    else {
+      if (arr[guess] < str) min = guess + 1;
+      else max = guess - 1;
+    }
+  }
+  return  -1
+};
+
+console.log(bynarySearh2(arr, 19));
