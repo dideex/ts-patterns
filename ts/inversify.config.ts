878a8870ebddf52d46b7df56ba38bf62a8ba60db
DiDex <wc34fun@mail.ru>
2019-01-11 10:30:57 +0800

    inversify fix meta data types

--- ts/inversify.config.ts
+++ ts/inversify.config.ts
@@ -4,9 +4,23 @@ import { IDepA, IDepB, IDepC } from './common'
 import { TYPES } from './types'
 import { ConcreteA, ConcreteB, ConcreteC } from './classes'
 
-const container = new Container()
-container.bind<IDepA>(TYPES.IDepA).to(ConcreteA)
-container.bind<IDepB>(TYPES.IDepB).to(ConcreteB)
-container.bind<IDepC>(TYPES.IDepC).to(ConcreteC)
+const container = new Container({
+  defaultScope: 'Singleton'
+})
+// IDepA will be singleton
+container
+  .bind<IDepA>(TYPES.IDepA)
+  .to(ConcreteA)
+  .inSingletonScope()
+// IDepB will be transient. We will get a new instance every time we request one
+container
+  .bind<IDepB>(TYPES.IDepB)
+  .to(ConcreteB)
+  .inTransientScope()
+// IDepC will have request scope. It will return the same instance until we call conainer.unbind
+container
+  .bind<IDepC>(TYPES.IDepC)
+  .to(ConcreteC)
+  .inRequestScope()
 
 export default container
