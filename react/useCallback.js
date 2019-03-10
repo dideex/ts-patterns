641b8fe220b5a6d77ddea26dac61463c698123ca
DiDex <wc34fun@mail.ru>
2019-03-10 17:58:53 +0800

    useForm

---
+++ react/useCallback.js
@@ -0,0 +1,28 @@
+const useForm = () => {
+  const [formState, setState] = useState({});
+  const onChange = useCallback((event) => {
+    setState((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    })
+  }, [setState]);
+  return [formState, onChange]
+}
+
+export default function Login() {
+  const [values, onChange] = useForm();
+  
+  return (
+   <form>
+    <input
+      type="email"
+      name="email"
+      value={values.email}
+      onChange={onChange} />
+    
+    <input
+      type="password"
+      name="password"
+      value={values.password}
+      onChange={onChange} />
+  ...
